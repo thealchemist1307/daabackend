cab69766c1816a44ed3732d738c46049905cee6f
TheAlchemist1307 <nishitrchouhan@gmail.com>
2021-03-17 03:46:03 +0530

    Changes Made

--- coordinates/views.py
+++ coordinates/views.py
@@ -2,12 +2,18 @@
 from django.shortcuts import render
 
 from django.http.response import JsonResponse
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
  
 from coordinates.models import Coordinates
 from coordinates.serializers import CoordinateSerializer
 from rest_framework.decorators import api_view
+import subprocess
+from subprocess import Popen, PIPE
+import pandas as pd
+import csv
+from django.http import HttpResponse
 
 
 @api_view(['GET','POST'])
@@ -18,10 +24,20 @@ def handle_coordinates(request):
         coordinate_serializer = CoordinateSerializer(coordinate, many=True)
         return JsonResponse(coordinate_serializer.data, safe=False)
     elif request.method == 'POST':
-        coordinate_data = JSONParser().parse(request)
-        coordinate_serializer = CoordinateSerializer(data=coordinate_data)
-        if coordinate_serializer.is_valid():
-            coordinate_serializer.save()
-            return JsonResponse(coordinate_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(coordinate_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        subprocess.call(["g++", "Test.cc"]) # OR gcc for c program
+        p = Popen(['./a.out'], shell=True, stdout=PIPE, stdin=PIPE)
+
+        value = str("2 \n 1 2 3 4 \n 4 5 6 7") + '\n'
+        value = bytes(value, 'UTF-8')  # Needed in Python 3.
+        p.stdin.write(value)
+        p.stdin.flush()
+        result = p.stdout.readline().strip()
+
+        with open('rectangles.csv') as csv_file:
+            response = HttpResponse(csv_file, content_type='text/csv',status=status.HTTP_201_CREATED)
+            filename = "rectangles.csv"
+            response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+
+        return response
+
  
